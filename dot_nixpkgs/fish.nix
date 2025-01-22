58b3d0db3a0e0f107219ed38e38e156045f72e8d
Cuichen Li <cuichen.li94@gmail.com>
2025-01-22 17:15:49 +0800

    let it work with linux as well

--- dot_nixpkgs/fish.nix
+++ dot_nixpkgs/fish.nix
@@ -1,34 +1,46 @@
 { pkgs, config, ...}:
 let 
   homeDir = "${config.home.homeDirectory}";
+  _plugins = [
+    {
+      name = "asdf";
+      src = pkgs.fetchFromGitHub {
+          owner = "rstacruz";
+          repo = "fish-asdf";
+          rev = "5869c1b1ecfba63f461abd8f98cb21faf337d004";
+          sha256 = "39L6UDslgIEymFsQY8klV/aluU971twRUymzRL17+6c=";
+      };
+    }
+  ];
+  nixShellPlugin = [
+    {
+      name = "nix-env";
+      src = pkgs.fetchFromGitHub {
+        owner = "lilyball";
+        repo = "nix-env.fish";
+        rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+        sha256 = "RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
+      };
+    }
+  ];
+  finalPlugins = if pkgs.stdenv.isLinux then _plugins ++ nixShellPlugin else _plugins; 
 in
 {
   programs.fish = {
     enable = true;
-    plugins = [
-        {
-            name = "asdf";
-            src = pkgs.fetchFromGitHub {
-                owner = "rstacruz";
-                repo = "fish-asdf";
-                rev = "5869c1b1ecfba63f461abd8f98cb21faf337d004";
-                sha256 = "39L6UDslgIEymFsQY8klV/aluU971twRUymzRL17+6c=";
-            };
-        }
-    ];
-    
 
+    plugins = finalPlugins;
+    
     shellInit = ''
         export PATH="${homeDir}/.asdf/shims:$PATH"
         source ${homeDir}/.asdf/plugins/java/set-java-home.fish
         eval "$(zoxide init fish)"
         fish_add_path /opt/homebrew/bin 
     '';
-      # set --universal tide_right_prompt_items status cmd_duration context jobs direnv node python rustc java php pulumi ruby go gcloud distrobox toolbox terraform aws nix_shell crystal elixir zig time
-#     source /Users/cuichli/.iterm2_shell_integration.fish
 
     shellInitLast = ''
       starship init fish | source
+      set -gx GPG_TTY (tty)
     '';
   };
 }
