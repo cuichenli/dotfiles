06b4d7df6a3696ea03dacfd5102a385f29cfc641
Cuichen Li <cuichen.li94@gmail.com>
2025-08-06 08:20:25 +0800

    update

--- dot_nixpkgs/personal-pkgs.nix
+++ dot_nixpkgs/personal-pkgs.nix
@@ -4,6 +4,21 @@ with pkgs; [
     protobuf
     pinentry
     unzip
-    emscripten
-    conan
+    minikube
+    k9s
+    kubernetes-helm
+    kubectl
+    pulumi
+    uv
+    glibc
+    google-cloud-sdk
+    jbang
+    temporal-cli
+    oci-cli
+    kubie
+    k3d
+    deno
+    gemini-cli
+    cargo
 ]
+
